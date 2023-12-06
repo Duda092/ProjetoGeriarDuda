82a4e0c49f07b10e0aa3ab39418bffa95da111e7
Maria <eduardacampos652@gmail.com>
2023-12-06 17:04:50 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+ const btn = document.getElementById('butao')
+
+
+ addEventListener('click', function validarFormulario(){
+    const nome = document.getElementById('nome').value
+    const email = document.getElementById('emailid').value
+    const senha = document.getElementById('senhaid').value
+    const telefone = document.getElementById('telefoneid').value
+    const endereco = document.getElementById('enderecoid').value
+    const identidade = document.getElementById('identidadeid').value
+    const cpf = document.getElementById('cpfid').value
+    const datanasc = document.getElementById('dataid').value
+    
+     
+    
+    
+
+
+
+ })
